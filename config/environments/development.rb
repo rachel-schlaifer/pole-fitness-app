6224dcbb2c9a47ab33e75b5dca24ff49a56b59f8
Rachel Schlaifer <rachelschlaifer9@gmail.com>
2024-09-22 19:23:18 +0900

    Set up Devise and Home controller.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -73,4 +73,27 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+# Ensure mailer works in development
+config.action_mailer.delivery_method = :smtp
+
+# Define default URL options (required by Devise)
+config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+# SMTP settings for Gmail or other email provider
+my_email = ENV['MY_EMAIL']
+
+password = ENV['EMAIL_PASSWORD']
+
+
+config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'yourdomain.com',
+  user_name:            my_email,
+  password:             password,          # replace with your email password
+  authentication:       'plain',
+  enable_starttls_auto: true
+}
+
 end
---
+++ todo.md
@@ -0,0 +1,27 @@
+# TO DO
+- Implement Devise
+- Write a core user journey
+
+# Features To Implement
+## Must haves
+- User authentication (Devise)
+- User profile
+- General Progress tracking
+- Trick progress tracking
+- Search for tricks
+- Content models (categories, level or difficulty)
+- Content management (adding/removing/listing)
+  - Who is adding content? Users? Instructors? Me?
+  - Different types of users?
+- Training area (you can input your own routine or have one suggested for you)
+
+## Nice to haves
+- Randomization of tricks and workouts available for users who aren't sure where to start
+
+## Could haves
+- Community aspect
+- Marketplace for pole gear
+- Search for pole studios near you
+
+## Stretch Goals
+- Pose Tracking
